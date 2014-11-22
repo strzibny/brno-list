da154c6fe96dc743c32e451393c0ba62cecabdb7
Petr Stribny <petrstribny@gmail.com>
2014-11-22 15:59:37 +0100

    Display opening hours on index

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     // load places from JSON file and put them in the page via templates
     $.getJSON("places.json", function(data) {
+        Handlebars.registerPartial("opening-hours", $("#opening-hours-partial").html());
+
         var template = $("#place-template").html();  
         var compileTemplate = Handlebars.compile(template);  
  
