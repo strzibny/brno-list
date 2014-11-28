9156c2640409c4f6785cc48d808f6445aff52e11
Petr Stribny <petrstribny@gmail.com>
2014-11-29 00:26:32 +0100

    Add now open/closed indicator

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
     // load places from JSON file and put them in the page via templates
     $.getJSON("places.json", function(data) {
+        Handlebars.registerHelper("isOpenNow", function(openingHours) {
+            var openTemplate = new Handlebars.SafeString($("#place-info-open").html());
+            var closedTemplate = new Handlebars.SafeString($("#place-info-closed").html());
+
+            return isOpenNow(openingHours) ? openTemplate : closedTemplate;
+        });
+
         Handlebars.registerPartial("opening-hours", $("#opening-hours-partial").html());
 
         var template = $("#place-template").html();  
