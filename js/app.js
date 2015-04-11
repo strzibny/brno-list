c057ce700c809cdf2745d3f702b9dab7bf0dc34f
Petr Stribny <pstribny@redhat.com>
2015-04-11 02:05:09 +0200

    Add search

--- js/app.js
+++ js/app.js
@@ -1,18 +1,40 @@
+var template,
+    compileTemplate,
+    data,
+    filteredData;
+
 $(document).ready(function() {
-    // load places from JSON file and put them in the page via templates
-    $.getJSON("data/places.json", function(data) {
-        Handlebars.registerHelper("isOpenNow", function(openingHours) {
-            var openTemplate = new Handlebars.SafeString($("#place-info-open").html());
-            var closedTemplate = new Handlebars.SafeString($("#place-info-closed").html());
-
-            return isOpenNow(openingHours) ? openTemplate : closedTemplate;
-        });
-
-        Handlebars.registerPartial("opening-hours", $("#opening-hours-partial").html());
-
-        var template = $("#place-template").html();  
-        var compileTemplate = Handlebars.compile(template);  
- 
-        $(document.body).append(compileTemplate(data));
+    
+    // register template helpers
+    Handlebars.registerHelper("isOpenNow", function(openingHours) {
+        var openTemplate = new Handlebars.SafeString($("#place-info-open").html());
+        var closedTemplate = new Handlebars.SafeString($("#place-info-closed").html());
+        
+        return isOpenNow(openingHours) ? openTemplate : closedTemplate;
     });
+    
+    Handlebars.registerPartial("opening-hours", $("#opening-hours-partial").html());
+    
+    template = $("#place-template").html();  
+    compileTemplate = Handlebars.compile(template);  
+    
+    // load places from JSON file and put them in the page
+    $.getJSON("data/places.json", function(receivedData) {
+        data = filteredData = receivedData;
+        render();
+    });    
+    
 });
+
+// filters list based on search term
+$("#search").on("input", function() {
+    filteredData = $.grep(data, function(element, index) {
+        return element.name.toLowerCase().indexOf($("#search").val().toLowerCase()) >= 0;
+    });
+    render();
+});
+
+// re-renders list of places
+function render() {
+    $("#places-placeholder").html(compileTemplate(filteredData));
+}
